11fada244b1806386635159c0123f08a03cb4e73
Shadofer <shadowrlrs@gmail.com>
2021-07-11 12:32:20 +0300

    Add file error handling.

--- assets/Scripts/Save.gd
+++ assets/Scripts/Save.gd
@@ -4,15 +4,23 @@ var file: File
 
 func _ready() -> void:
 	file = File.new()
+	
+	check_file()
 
+func check_file() -> void:
 	if not file.file_exists(Variables.SAVE_FILENAME):
-		file.open(Variables.SAVE_FILENAME, File.WRITE)
-
+		var file_result = file.open(Variables.SAVE_FILENAME, File.WRITE)
+		
+		if file_result == FAILED || file_result == ERR_FILE_NO_PERMISSION || file_result == ERR_FILE_ALREADY_IN_USE || file_result == ERR_FILE_CANT_WRITE:
+			Utils.log('Error occured while attempting to create file, aborting.')
+			get_tree().quit()
+			
 		file.store_line(JSON.print({}))
-
 		file.close()
 
 func add_value(key: String, value, set: bool = false) -> void:
+	check_file()
+	
 	var file_dict = get_file_dict()
 	
 	if key in file_dict.keys() and not set:
@@ -29,6 +37,8 @@ func add_value(key: String, value, set: bool = false) -> void:
 	file.close()
 
 func remove_value(key: String) -> void:
+	check_file()
+	
 	var file_dict = get_file_dict()
 	
 	if not key in file_dict:
@@ -43,6 +53,8 @@ func remove_value(key: String) -> void:
 	file.close()
 
 func get_value(key: String):
+	check_file()
+	
 	var values_dict = get_file_dict()
 
 	if key in values_dict:
@@ -50,6 +62,8 @@ func get_value(key: String):
 	return null
 
 func get_file_dict() -> Dictionary:
+	check_file()
+	
 	file.open(Variables.SAVE_FILENAME, File.READ_WRITE)
 	
 	return JSON.parse(file.get_as_text()).result
